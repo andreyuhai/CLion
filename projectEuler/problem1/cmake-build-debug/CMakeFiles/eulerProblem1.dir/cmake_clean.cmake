397744d2c03692de6a46f92ad682cd52df85cf8d
Burak <porfavordama@hotmail.com>
2018-03-07 02:10:47 +0300

    Project Euler Answers

---
+++ projectEuler/problem1/cmake-build-debug/CMakeFiles/eulerProblem1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eulerProblem1.dir/main.c.o"
+  "eulerProblem1.pdb"
+  "eulerProblem1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/eulerProblem1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
