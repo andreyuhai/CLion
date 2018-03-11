d4084273de96156f2bcd4705d6bdfc239e36e31f
Burak <porfavordama@hotmail.com>
2018-03-11 19:29:53 +0300

    Guessing random numbers game

---
+++ algorithmHomework/cmake-build-debug/CMakeFiles/algorithmHomework.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithmHomework.dir/main.c.o"
+  "algorithmHomework.pdb"
+  "algorithmHomework"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algorithmHomework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
